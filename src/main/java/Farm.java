f0c0cae12ebcff23598137ea8eb701c8897b4668
aozturk1 <aozturk1@asu.edu>
2023-04-20 19:04:09 -0700

    WIP implemented almost all requirements

--- src/main/java/Farm.java
+++ src/main/java/Farm.java
@@ -5,25 +5,26 @@ abstract public class Farm {
     // constructor, getters and setters for currency, farmers, animals and crops
 
     public abstract void upgrade();
-    public abstract int getPassiveCurrency();
+    public abstract int getPassiveCurrency(int i);
     public abstract boolean getCapacity();
 
-    public abstract void runCycle();
+    public abstract void runCycle(int i);
     public abstract void addObservers(Observer observer);
     public abstract void tellObservers();
+    public abstract void generateRandomEvent(Main.RandomEventType eventType);
 }
 
 class AnimalFarm extends Farm {
     private List<Observer> contacts = new ArrayList<Observer>();
     public int animals;
-    public int cpacity;
+    public int capacity;
     private int passiveCurrency;
     private int upgradeCost;
     private int farmLevel;
 
     public AnimalFarm() {
         this.animals = 2;
-        this.cpacity = 5;
+        this.capacity = 5;
         this.passiveCurrency = 1;
         this.upgradeCost = 10;
         this.farmLevel = 1;
@@ -37,29 +38,34 @@ class AnimalFarm extends Farm {
             this.farmLevel++;
             Output.slowPrint("FARM LEVEL UP >>> This farm is now level: " + this.farmLevel);
         } else {
-            Output.slowPrint("The farm is " + (2*(this.upgradeCost) - CurrencyManager.getInstance().getCurrency()) + " Farm Coins away from leveling up.");
-            if (this.animals < 10) {
+            Output.slowPrint((2*(this.upgradeCost) - CurrencyManager.getInstance().getCurrency()) + " Farm Coins away from leveling up.");
+            if (this.animals <= this.capacity/3) {
                 tellObservers();
             }
         }
     }
 
     @Override
-    public int getPassiveCurrency() {
+    public int getPassiveCurrency(int i) {
         return this.animals * this.passiveCurrency;
     }
 
     @Override
     public boolean getCapacity() {
-        if(this.animals >= this.cpacity){
+        if(this.animals >= this.capacity){
             Output.slowPrint("Farm reached animal its maximum animal capacity!");
             return true;
         } return false;
     }
 
     @Override
-    public void runCycle() {
-        this.animals++;
+    public void runCycle(int i) {
+        Output.slowPrint("Feeding and herding animals...");
+        Output.slowPrint("Operating farm equipment...");
+        Output.slowPrint("Collecting animal products...");
+        if (this.animals >= 2 && ( i % 4 == 0 )) {
+            this.animals++;
+        }
     }
 
     @Override
@@ -73,16 +79,40 @@ class AnimalFarm extends Farm {
             o.observableNotify();
         }
     }
+
+    @Override
+    public void generateRandomEvent(Main.RandomEventType eventType) {
+        switch (eventType) {
+            case DISEASE_OUTBREAK:
+                System.out.println("Animals died due to a disease outbreak!");
+                break;
+            case WEATHER_STORM:
+                Output.slowPrint("A storm caused killed animals!");
+                break;
+            case MARKET_CRASH:
+                Output.slowPrint("Oh no, a market crash caused a currency loss!");
+                break;
+            case EQUIPMENT_EXPENSES:
+                Output.slowPrint("Bummer, equipment expenses were paid!");
+                break;
+            case ALIEN_ABDUCTION:
+                Output.slowPrint("YOU GOT ABDUCTED BY ALIENS. BETTER LUCK NEX TIME!");
+                Output.slowPrint("!!!!!!!!GAME OVER!!!!!!!!");
+                System.exit(0);
+        }
+    }
 }
 
 class CropFarm extends Farm {
-    private int crops;
+    private List<Observer> contacts = new ArrayList<Observer>();
+    public int capacity;
+    public int crops;
     private int passiveCurrency;
     private int upgradeCost;
     private int farmLevel;
 
     public CropFarm() {
-        //this.funds = 10;
+        this.capacity = 3;
         this.crops = 1;
         this.passiveCurrency = 2;
         this.upgradeCost = 5;
@@ -95,46 +125,85 @@ class CropFarm extends Farm {
             CurrencyManager.getInstance().spendCurrency(this.upgradeCost);
             this.upgradeCost *= 2;
             this.farmLevel++;
+            this.capacity++;
             Output.slowPrint("FARM LEVEL UP >>> This farm is now level: " + this.farmLevel);
         } else {
-            Output.slowPrint("The farm is " + (2*(this.upgradeCost) - CurrencyManager.getInstance().getCurrency()) + " Farm Coins away from leveling up.");
+            Output.slowPrint((2*(this.upgradeCost) - CurrencyManager.getInstance().getCurrency()) + " Farm Coins away from leveling up.");
+            if (this.crops <= this.capacity/3) {
+                tellObservers();
+            }
         }
     }
 
     @Override
-    public int getPassiveCurrency() {
-        return this.crops * this.passiveCurrency;
+    public int getPassiveCurrency(int i) {
+        if ( i % 3 == 0){
+            Output.slowPrint("Crops are being harvested...");
+            return this.passiveCurrency * this.crops;
+        }
+        Output.slowPrint("Crops not ready to harvest!");
+        return 1;
     }
 
     @Override
     public boolean getCapacity() {
-        return false;
+        if(this.crops >= this.capacity){
+            Output.slowPrint("Farm reached its maximum crop capacity!");
+            return true;
+        } return false;
     }
 
     @Override
-    public void runCycle() {
-
+    public void runCycle(int i) {
+        Output.slowPrint("Operating farm equipment...");
+        if (i >= 0){
+            Output.slowPrint("Crops are being watered...");
+        }
     }
 
     @Override
     public void addObservers(Observer observer) {
-
+        contacts.add(observer);
     }
 
     @Override
     public void tellObservers() {
-
+        for (Observer o: contacts) {
+            o.observableNotify();
+        }
     }
-    // implementation of CropFarm
+
+    @Override
+    public void generateRandomEvent(Main.RandomEventType eventType) {
+        switch (eventType) {
+            case DISEASE_OUTBREAK:
+                System.out.println("Some crops died due to a disease outbreak!");
+                break;
+            case WEATHER_STORM:
+                Output.slowPrint("A storm caused crop damage.");
+                break;
+            case MARKET_CRASH:
+                Output.slowPrint("A market crash caused a currency loss of.");
+                break;
+            case EQUIPMENT_EXPENSES:
+                Output.slowPrint("Bummer, equipment expenses were paid!");
+                break;
+            case ALIEN_ABDUCTION:
+                Output.slowPrint("YOU GOT ABDUCTED BY ALIENS. BETTER LUCK NEX TIME!");
+                Output.slowPrint("!!!!!!!!GAME OVER!!!!!!!!");
+                System.exit(0);
+        }
+    }
 }
 
 class FarmFactory {
-    public static Farm createFarm(String farmType) {
-        if (farmType.equals("animal")) {
+    public static Farm createFarm(double farmType) {
+        if (farmType < 0.5) {
+            Output.slowPrint("Great choice, an animal farm!");
             return new AnimalFarm();
-        } else if (farmType.equals("crop")) {
+        } else {
+            Output.slowPrint("Great choice, a crop farm!");
             return new CropFarm();
         }
-        return null;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,50 +9,56 @@ public class Main {
 
         CurrencyManager.getInstance();
 
-        // create a new AnimalFarm
-        Farm animalFarm = FarmFactory.createFarm("animal");
+        // create a new Farm
+        double random = Math.random();
+        Farm farm = FarmFactory.createFarm(random);
 
         // add supplier to contacts
         // add news to contacts
         News news = new News("Local Whether 33");
         Supplier supplier = new Supplier("Sarah");
-        animalFarm.addObservers(news);
-        animalFarm.addObservers(supplier);
+        farm.addObservers(news);
+        farm.addObservers(supplier);
 
         // simulate the farm for infinite cycles
         int i = 0;
+        boolean crashNBurn = false;
+        RandomEventType type = null;
         while ( true ) {
             //DAY CYCLE STARTS
 
             Output.slowPrint("Day " + (i+1) + " begins.");
 
             Output.slowPrint("Reading the newspaper...");
-            RandomEventType type = getRandomEventType();
-            // run a cycle on the animalFarm
-            Output.slowPrint("Feeding and herding animals...");
-            Output.slowPrint("Operating farm equipment...");
-            Output.slowPrint("Collecting animal products...");
-            animalFarm.runCycle();
+            if (random < 0.5) {
+                type = getRandomEventType();
+                crashNBurn = true;
+            }
+            // run a cycle on the Farm
+            farm.runCycle(i);
 
-            // add passive currency to the animalFarm
-            int passiveCurrency = animalFarm.getPassiveCurrency();
+            // add passive currency to the Farm
+            int passiveCurrency = farm.getPassiveCurrency(i);
             Output.slowPrint("Today the farm earned " + passiveCurrency + " Farm Coins!");
             CurrencyManager.getInstance().addCurrency(passiveCurrency);
 
             // print out the current currency
             Output.slowPrint("Current currency: " + CurrencyManager.getInstance().getCurrency());
 
-            // upgrade the animalFarm if possible
-            animalFarm.upgrade();
+            // upgrade the Farm if possible
+            farm.upgrade();
 
             //DAY CYCLE ENDS -> NIGHT CYCLE STARTS
             Output.slowPrint("The sun sets...");
 
+            if (crashNBurn) {
+                farm.generateRandomEvent(type);
+            }
 
             //NIGHT CYCLE ENDS
 
             Output.slowPrint("Day " + (i+1) + " ends.");
-            Output.slowPrint(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Output.slowPrint(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
             i++;
         }
 
@@ -73,32 +79,6 @@ public class Main {
         Output.slowPrint("There is a chance of " + RandomEventType.values()[eventIndex]);
         return RandomEventType.values()[eventIndex];
     }
-
-//    public void generateRandomEvent(RandomEventType eventType) {
-//        //RandomEventType eventType = getRandomEventType();
-//        switch (eventType) {
-//            case DISEASE_OUTBREAK:
-//                int numAffectedAnimals = getRandomNumber(1, 10);
-//                int numDeadAnimals = Math.min(numAffectedAnimals, animals.size());
-//                for (int i = 0; i < numDeadAnimals; i++) {
-//                    animals.remove(getRandomNumber(0, animals.size() - 1));
-//                }
-//                System.out.println(numDeadAnimals + " animals died due to a disease outbreak.");
-//                break;
-//            case WEATHER_STORM:
-//                double cropDamage = getRandomNumber(0.1, 0.5);
-//                for (Crop crop : crops) {
-//                    crop.reduceGrowth(cropDamage);
-//                }
-//                System.out.println("A storm caused " + (cropDamage * 100) + "% crop damage.");
-//                break;
-//            case MARKET_CRASH:
-//                double currencyLoss = getRandomNumber(0.1, 0.5) * currency;
-//                currency -= currencyLoss;
-//                System.out.println("A market crash caused a currency loss of " + currencyLoss + ".");
-//                break;
-//        }
-//    }
 
     //testing test method
     public static int factorial(int n) {
